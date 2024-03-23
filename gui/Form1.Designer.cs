7a4e51fd81fe4f9acf1c8cff7b66f561a296c559
BAD-AL <christopherbadal@yahoo.com>
2024-03-22 18:06:03 -0600

    Restoring file menu, deleting un-necessary config file

--- gui/Form1.Designer.cs
+++ gui/Form1.Designer.cs
@@ -35,6 +35,10 @@ namespace gui
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RepackForm));
 			this.lvlsList = new System.Windows.Forms.ListBox();
+			this.contextMenu_lvlList = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.pickFolderToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.removeSelectedToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.clearItemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.label1 = new System.Windows.Forms.Label();
 			this.outputDisplay = new System.Windows.Forms.TextBox();
 			this.pickFolder = new System.Windows.Forms.Button();
@@ -45,22 +49,26 @@ namespace gui
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.pickFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.removeSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.clearFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.lvlpackOnGitHubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.aboutLvlpackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.removeSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.contextMenu_lvlList = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.removeSelectedToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-			this.pickFolderToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-			this.clearItemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.pickFolderToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.removeSelectedToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.clearItemsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.gitHubRepoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutLvlrepackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenu_lvlList.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
 			this.splitContainer1.Panel1.SuspendLayout();
 			this.splitContainer1.Panel2.SuspendLayout();
 			this.splitContainer1.SuspendLayout();
 			this.menuStrip1.SuspendLayout();
-			this.contextMenu_lvlList.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// lvlsList
@@ -78,15 +86,46 @@ namespace gui
 			this.lvlsList.DragDrop += new System.Windows.Forms.DragEventHandler(this.lvlsList_DragDrop);
 			this.lvlsList.DragEnter += new System.Windows.Forms.DragEventHandler(this.lvlsList_DragEnter);
 			// 
+			// contextMenu_lvlList
+			// 
+			this.contextMenu_lvlList.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.contextMenu_lvlList.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pickFolderToolStripMenuItem1,
+            this.removeSelectedToolStripMenuItem1,
+            this.clearItemsToolStripMenuItem});
+			this.contextMenu_lvlList.Name = "contextMenu_lvlList";
+			this.contextMenu_lvlList.Size = new System.Drawing.Size(220, 100);
+			// 
+			// pickFolderToolStripMenuItem1
+			// 
+			this.pickFolderToolStripMenuItem1.Name = "pickFolderToolStripMenuItem1";
+			this.pickFolderToolStripMenuItem1.Size = new System.Drawing.Size(219, 32);
+			this.pickFolderToolStripMenuItem1.Text = "&Pick Folder";
+			this.pickFolderToolStripMenuItem1.Click += new System.EventHandler(this.pickFolder_Click);
+			// 
+			// removeSelectedToolStripMenuItem1
+			// 
+			this.removeSelectedToolStripMenuItem1.Name = "removeSelectedToolStripMenuItem1";
+			this.removeSelectedToolStripMenuItem1.Size = new System.Drawing.Size(219, 32);
+			this.removeSelectedToolStripMenuItem1.Text = "&Remove Selected";
+			this.removeSelectedToolStripMenuItem1.Click += new System.EventHandler(this.removeSelectedToolStripMenuItem_Click);
+			// 
+			// clearItemsToolStripMenuItem
+			// 
+			this.clearItemsToolStripMenuItem.Name = "clearItemsToolStripMenuItem";
+			this.clearItemsToolStripMenuItem.Size = new System.Drawing.Size(219, 32);
+			this.clearItemsToolStripMenuItem.Text = "&Clear Items";
+			this.clearItemsToolStripMenuItem.Click += new System.EventHandler(this.clearFilesMenuItem_Click);
+			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(15, 33);
 			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(111, 20);
+			this.label1.Size = new System.Drawing.Size(341, 20);
 			this.label1.TabIndex = 1;
-			this.label1.Text = ".lvls to Repack";
+			this.label1.Text = ".lvls to Repack (drag && drop files. folders below)";
 			// 
 			// outputDisplay
 			// 
@@ -173,8 +212,8 @@ namespace gui
 			this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
 			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
 			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.aboutToolStripMenuItem});
+            this.fileToolStripMenuItem1,
+            this.aboutToolStripMenuItem1});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
 			this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
@@ -190,27 +229,34 @@ namespace gui
             this.clearFilesToolStripMenuItem,
             this.exitToolStripMenuItem});
 			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 32);
 			this.fileToolStripMenuItem.Text = "File";
 			// 
 			// pickFolderToolStripMenuItem
 			// 
 			this.pickFolderToolStripMenuItem.Name = "pickFolderToolStripMenuItem";
-			this.pickFolderToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.pickFolderToolStripMenuItem.Size = new System.Drawing.Size(249, 34);
 			this.pickFolderToolStripMenuItem.Text = "&Pick Folder";
 			this.pickFolderToolStripMenuItem.Click += new System.EventHandler(this.pickFolder_Click);
+			// 
+			// removeSelectedToolStripMenuItem
+			// 
+			this.removeSelectedToolStripMenuItem.Name = "removeSelectedToolStripMenuItem";
+			this.removeSelectedToolStripMenuItem.Size = new System.Drawing.Size(249, 34);
+			this.removeSelectedToolStripMenuItem.Text = "&Remove Selected";
+			this.removeSelectedToolStripMenuItem.Click += new System.EventHandler(this.removeSelectedToolStripMenuItem_Click);
 			// 
 			// clearFilesToolStripMenuItem
 			// 
 			this.clearFilesToolStripMenuItem.Name = "clearFilesToolStripMenuItem";
-			this.clearFilesToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.clearFilesToolStripMenuItem.Size = new System.Drawing.Size(249, 34);
 			this.clearFilesToolStripMenuItem.Text = "&Clear files";
 			this.clearFilesToolStripMenuItem.Click += new System.EventHandler(this.clearFilesMenuItem_Click);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(249, 34);
 			this.exitToolStripMenuItem.Text = "E&xit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
 			// 
@@ -220,60 +266,84 @@ namespace gui
             this.lvlpackOnGitHubToolStripMenuItem,
             this.aboutLvlpackToolStripMenuItem});
 			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 32);
 			this.aboutToolStripMenuItem.Text = "&About";
 			// 
 			// lvlpackOnGitHubToolStripMenuItem
 			// 
 			this.lvlpackOnGitHubToolStripMenuItem.Name = "lvlpackOnGitHubToolStripMenuItem";
-			this.lvlpackOnGitHubToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.lvlpackOnGitHubToolStripMenuItem.Size = new System.Drawing.Size(262, 34);
 			this.lvlpackOnGitHubToolStripMenuItem.Text = "lvl-pack on GitHub";
 			this.lvlpackOnGitHubToolStripMenuItem.Click += new System.EventHandler(this.lvlpackOnGitHubToolStripMenuItem_Click);
 			// 
 			// aboutLvlpackToolStripMenuItem
 			// 
 			this.aboutLvlpackToolStripMenuItem.Name = "aboutLvlpackToolStripMenuItem";
-			this.aboutLvlpackToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.aboutLvlpackToolStripMenuItem.Size = new System.Drawing.Size(262, 34);
 			this.aboutLvlpackToolStripMenuItem.Text = "About lvl-pack";
 			this.aboutLvlpackToolStripMenuItem.Click += new System.EventHandler(this.aboutLvlpackToolStripMenuItem_Click);
 			// 
-			// removeSelectedToolStripMenuItem
+			// fileToolStripMenuItem1
 			// 
-			this.removeSelectedToolStripMenuItem.Name = "removeSelectedToolStripMenuItem";
-			this.removeSelectedToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
-			this.removeSelectedToolStripMenuItem.Text = "&Remove Selected";
-			this.removeSelectedToolStripMenuItem.Click += new System.EventHandler(this.removeSelectedToolStripMenuItem_Click);
+			this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pickFolderToolStripMenuItem2,
+            this.removeSelectedToolStripMenuItem2,
+            this.clearItemsToolStripMenuItem1,
+            this.exitToolStripMenuItem1});
+			this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+			this.fileToolStripMenuItem1.Size = new System.Drawing.Size(54, 29);
+			this.fileToolStripMenuItem1.Text = "File";
 			// 
-			// contextMenu_lvlList
+			// pickFolderToolStripMenuItem2
 			// 
-			this.contextMenu_lvlList.ImageScalingSize = new System.Drawing.Size(24, 24);
-			this.contextMenu_lvlList.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.pickFolderToolStripMenuItem1,
-            this.removeSelectedToolStripMenuItem1,
-            this.clearItemsToolStripMenuItem});
-			this.contextMenu_lvlList.Name = "contextMenu_lvlList";
-			this.contextMenu_lvlList.Size = new System.Drawing.Size(220, 100);
+			this.pickFolderToolStripMenuItem2.Name = "pickFolderToolStripMenuItem2";
+			this.pickFolderToolStripMenuItem2.Size = new System.Drawing.Size(270, 34);
+			this.pickFolderToolStripMenuItem2.Text = "Pick Folder";
+			this.pickFolderToolStripMenuItem2.Click += new System.EventHandler(this.pickFolder_Click);
 			// 
-			// removeSelectedToolStripMenuItem1
+			// removeSelectedToolStripMenuItem2
 			// 
-			this.removeSelectedToolStripMenuItem1.Name = "removeSelectedToolStripMenuItem1";
-			this.removeSelectedToolStripMenuItem1.Size = new System.Drawing.Size(219, 32);
-			this.removeSelectedToolStripMenuItem1.Text = "&Remove Selected";
-			this.removeSelectedToolStripMenuItem1.Click += new System.EventHandler(this.removeSelectedToolStripMenuItem_Click);
+			this.removeSelectedToolStripMenuItem2.Name = "removeSelectedToolStripMenuItem2";
+			this.removeSelectedToolStripMenuItem2.Size = new System.Drawing.Size(270, 34);
+			this.removeSelectedToolStripMenuItem2.Text = "&Remove Selected";
+			this.removeSelectedToolStripMenuItem2.Click += new System.EventHandler(this.removeSelectedToolStripMenuItem_Click);
 			// 
-			// pickFolderToolStripMenuItem1
+			// clearItemsToolStripMenuItem1
 			// 
-			this.pickFolderToolStripMenuItem1.Name = "pickFolderToolStripMenuItem1";
-			this.pickFolderToolStripMenuItem1.Size = new System.Drawing.Size(219, 32);
-			this.pickFolderToolStripMenuItem1.Text = "&Pick Folder";
-			this.pickFolderToolStripMenuItem1.Click += new System.EventHandler(this.pickFolder_Click);
+			this.clearItemsToolStripMenuItem1.Name = "clearItemsToolStripMenuItem1";
+			this.clearItemsToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+			this.clearItemsToolStripMenuItem1.Text = "&Clear Items";
+			this.clearItemsToolStripMenuItem1.Click += new System.EventHandler(this.clearFilesMenuItem_Click);
 			// 
-			// clearItemsToolStripMenuItem
+			// exitToolStripMenuItem1
 			// 
-			this.clearItemsToolStripMenuItem.Name = "clearItemsToolStripMenuItem";
-			this.clearItemsToolStripMenuItem.Size = new System.Drawing.Size(219, 32);
-			this.clearItemsToolStripMenuItem.Text = "&Clear Items";
-			this.clearItemsToolStripMenuItem.Click += new System.EventHandler(this.clearFilesMenuItem_Click);
+			this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+			this.exitToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+			this.exitToolStripMenuItem1.Text = "E&xit";
+			this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// aboutToolStripMenuItem1
+			// 
+			this.aboutToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gitHubRepoToolStripMenuItem,
+            this.aboutLvlrepackToolStripMenuItem});
+			this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+			this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(78, 29);
+			this.aboutToolStripMenuItem1.Text = "About";
+			// 
+			// gitHubRepoToolStripMenuItem
+			// 
+			this.gitHubRepoToolStripMenuItem.Name = "gitHubRepoToolStripMenuItem";
+			this.gitHubRepoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.gitHubRepoToolStripMenuItem.Text = "GitHub Repo";
+			this.gitHubRepoToolStripMenuItem.Click += new System.EventHandler(this.lvlpackOnGitHubToolStripMenuItem_Click);
+			// 
+			// aboutLvlrepackToolStripMenuItem
+			// 
+			this.aboutLvlrepackToolStripMenuItem.Name = "aboutLvlrepackToolStripMenuItem";
+			this.aboutLvlrepackToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+			this.aboutLvlrepackToolStripMenuItem.Text = "&About lvl-repack";
+			this.aboutLvlrepackToolStripMenuItem.Click += new System.EventHandler(this.aboutLvlpackToolStripMenuItem_Click);
 			// 
 			// RepackForm
 			// 
@@ -292,6 +362,7 @@ namespace gui
 			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "RepackForm";
 			this.Text = "lvl-repack";
+			this.contextMenu_lvlList.ResumeLayout(false);
 			this.splitContainer1.Panel1.ResumeLayout(false);
 			this.splitContainer1.Panel2.ResumeLayout(false);
 			this.splitContainer1.Panel2.PerformLayout();
@@ -299,7 +370,6 @@ namespace gui
 			this.splitContainer1.ResumeLayout(false);
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
-			this.contextMenu_lvlList.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -328,5 +398,13 @@ namespace gui
 		private ToolStripMenuItem pickFolderToolStripMenuItem1;
 		private ToolStripMenuItem removeSelectedToolStripMenuItem1;
 		private ToolStripMenuItem clearItemsToolStripMenuItem;
+		private ToolStripMenuItem fileToolStripMenuItem1;
+		private ToolStripMenuItem pickFolderToolStripMenuItem2;
+		private ToolStripMenuItem removeSelectedToolStripMenuItem2;
+		private ToolStripMenuItem clearItemsToolStripMenuItem1;
+		private ToolStripMenuItem exitToolStripMenuItem1;
+		private ToolStripMenuItem aboutToolStripMenuItem1;
+		private ToolStripMenuItem gitHubRepoToolStripMenuItem;
+		private ToolStripMenuItem aboutLvlrepackToolStripMenuItem;
 	}
 }
