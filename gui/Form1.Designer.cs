d8fefb36ee501e75d6d3f0beff50c6afa90b7e6b
BAD-AL <BAD-AL@users.noreply.github.com>
2024-03-25 15:50:05 -0600

    Fix for file names with spaces; Form re-sizing, show progress, added file menu (#2)
    * Fixes for:
     Form re-sizing
     no progress shown
     filenames with spaces don't work
    Added:
     File Menu
     'Clear Items' functionality
    
    * 1. Changed to .NET Framework 4.6 to increase compatibility
    2. added Drag & Drop
    3. Added context & other menus for lvl list box
    4. Added compression icon for app
    5. Added Tool Tip for list box
    6. Added message for amount reduced for output
    7. Added remove item menu item to menus
    8. Added about menu
    
    * 1. Removed un-necessary files.
    2. restored gui.csproj and deleted extra solution
    3. better organized Form1
    
    * Restoring file menu, deleting un-necessary config file
    
    * Back to .net 8
    
    * adding this back with relative path
    
    * small formatting improvement to repackButton_Click
    
    * uncomment ApplicationConfiguration.Initialize();
    
    I'm not sure why this is causing compilation to fail. But without the app seems to lose some of it's theming and some UI elements start looking (to me) like the old Windows Classic theme. I think for users it's better to have the buttons looking and it should be no trouble for you to comment it out again before building it yourself if you need.
    
    ---------
    
    Co-authored-by: PrismaticFlower <5582558+PrismaticFlower@users.noreply.github.com>

--- gui/Form1.Designer.cs
+++ gui/Form1.Designer.cs
@@ -28,48 +28,110 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Repack));
 			lvlsList = new ListBox();
+			contextMenu_lvlFiles = new ContextMenuStrip(components);
+			clearItemsToolStripMenuItem = new ToolStripMenuItem();
+			removeSelectedItemToolStripMenuItem = new ToolStripMenuItem();
+			pickFolderToolStripMenuItem = new ToolStripMenuItem();
 			label1 = new Label();
 			outputDisplay = new TextBox();
 			pickFolder = new Button();
 			repackButton = new Button();
 			preferCompressedTextures = new CheckBox();
 			repackProgress = new ProgressBar();
+			splitContainer1 = new SplitContainer();
+			menuStrip1 = new MenuStrip();
+			fileToolStripMenuItem = new ToolStripMenuItem();
+			clearItemsToolStripMenuItem1 = new ToolStripMenuItem();
+			removeSelectedToolStripMenuItem = new ToolStripMenuItem();
+			pickFolderToolStripMenuItem1 = new ToolStripMenuItem();
+			exitToolStripMenuItem = new ToolStripMenuItem();
+			aboutToolStripMenuItem = new ToolStripMenuItem();
+			gitHubRepoToolStripMenuItem = new ToolStripMenuItem();
+			aboutToolStripMenuItem1 = new ToolStripMenuItem();
+			contextMenu_lvlFiles.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+			splitContainer1.Panel1.SuspendLayout();
+			splitContainer1.Panel2.SuspendLayout();
+			splitContainer1.SuspendLayout();
+			menuStrip1.SuspendLayout();
 			SuspendLayout();
 			// 
 			// lvlsList
 			// 
+			lvlsList.AllowDrop = true;
+			lvlsList.ContextMenuStrip = contextMenu_lvlFiles;
+			lvlsList.Dock = DockStyle.Fill;
 			lvlsList.FormattingEnabled = true;
-			lvlsList.ItemHeight = 15;
-			lvlsList.Location = new Point(12, 29);
+			lvlsList.ItemHeight = 25;
+			lvlsList.Location = new Point(0, 0);
+			lvlsList.Margin = new Padding(4, 5, 4, 5);
 			lvlsList.Name = "lvlsList";
-			lvlsList.Size = new Size(345, 379);
+			lvlsList.SelectionMode = SelectionMode.MultiSimple;
+			lvlsList.Size = new Size(386, 360);
 			lvlsList.TabIndex = 0;
+			lvlsList.DragDrop += lvlsList_DragDrop;
+			lvlsList.DragEnter += lvlsList_DragEnter;
+			// 
+			// contextMenu_lvlFiles
+			// 
+			contextMenu_lvlFiles.ImageScalingSize = new Size(24, 24);
+			contextMenu_lvlFiles.Items.AddRange(new ToolStripItem[] { clearItemsToolStripMenuItem, removeSelectedItemToolStripMenuItem, pickFolderToolStripMenuItem });
+			contextMenu_lvlFiles.Name = "contextMenu_lvlFiles";
+			contextMenu_lvlFiles.Size = new Size(266, 100);
+			// 
+			// clearItemsToolStripMenuItem
+			// 
+			clearItemsToolStripMenuItem.Name = "clearItemsToolStripMenuItem";
+			clearItemsToolStripMenuItem.Size = new Size(265, 32);
+			clearItemsToolStripMenuItem.Text = "&Clear Items";
+			clearItemsToolStripMenuItem.Click += clearItemsToolStripMenuItem_Click;
+			// 
+			// removeSelectedItemToolStripMenuItem
+			// 
+			removeSelectedItemToolStripMenuItem.Name = "removeSelectedItemToolStripMenuItem";
+			removeSelectedItemToolStripMenuItem.Size = new Size(265, 32);
+			removeSelectedItemToolStripMenuItem.Text = "&Remove selected items";
+			removeSelectedItemToolStripMenuItem.Click += removeSelectedItemToolStripMenuItem_Click;
+			// 
+			// pickFolderToolStripMenuItem
+			// 
+			pickFolderToolStripMenuItem.Name = "pickFolderToolStripMenuItem";
+			pickFolderToolStripMenuItem.Size = new Size(265, 32);
+			pickFolderToolStripMenuItem.Text = "&Pick Folder";
+			pickFolderToolStripMenuItem.Click += pickFolder_Click;
 			// 
 			// label1
 			// 
 			label1.AutoSize = true;
 			label1.Font = new Font("Segoe UI", 11F);
-			label1.Location = new Point(12, 9);
+			label1.Location = new Point(17, 46);
+			label1.Margin = new Padding(4, 0, 4, 0);
 			label1.Name = "label1";
-			label1.Size = new Size(103, 20);
+			label1.Size = new Size(149, 30);
 			label1.TabIndex = 1;
 			label1.Text = ".lvls to Repack";
 			// 
 			// outputDisplay
 			// 
-			outputDisplay.Location = new Point(363, 29);
+			outputDisplay.Dock = DockStyle.Fill;
+			outputDisplay.Location = new Point(0, 0);
+			outputDisplay.Margin = new Padding(4, 5, 4, 5);
 			outputDisplay.Multiline = true;
 			outputDisplay.Name = "outputDisplay";
 			outputDisplay.ReadOnly = true;
-			outputDisplay.Size = new Size(425, 379);
+			outputDisplay.Size = new Size(432, 360);
 			outputDisplay.TabIndex = 2;
 			// 
 			// pickFolder
 			// 
-			pickFolder.Location = new Point(12, 415);
+			pickFolder.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+			pickFolder.Location = new Point(17, 447);
+			pickFolder.Margin = new Padding(4, 5, 4, 5);
 			pickFolder.Name = "pickFolder";
-			pickFolder.Size = new Size(75, 23);
+			pickFolder.Size = new Size(107, 38);
 			pickFolder.TabIndex = 3;
 			pickFolder.Text = "Pick Folder";
 			pickFolder.UseVisualStyleBackColor = true;
@@ -77,10 +139,12 @@
 			// 
 			// repackButton
 			// 
+			repackButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
 			repackButton.Enabled = false;
-			repackButton.Location = new Point(93, 415);
+			repackButton.Location = new Point(133, 447);
+			repackButton.Margin = new Padding(4, 5, 4, 5);
 			repackButton.Name = "repackButton";
-			repackButton.Size = new Size(75, 23);
+			repackButton.Size = new Size(107, 38);
 			repackButton.TabIndex = 4;
 			repackButton.Text = "Repack";
 			repackButton.UseVisualStyleBackColor = true;
@@ -88,35 +152,135 @@
 			// 
 			// preferCompressedTextures
 			// 
+			preferCompressedTextures.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
 			preferCompressedTextures.AutoSize = true;
-			preferCompressedTextures.Location = new Point(174, 418);
+			preferCompressedTextures.Checked = true;
+			preferCompressedTextures.CheckState = CheckState.Checked;
+			preferCompressedTextures.Location = new Point(249, 452);
+			preferCompressedTextures.Margin = new Padding(4, 5, 4, 5);
 			preferCompressedTextures.Name = "preferCompressedTextures";
-			preferCompressedTextures.Size = new Size(172, 19);
+			preferCompressedTextures.Size = new Size(257, 29);
 			preferCompressedTextures.TabIndex = 5;
 			preferCompressedTextures.Text = "Prefer Compressed Textures";
 			preferCompressedTextures.UseVisualStyleBackColor = true;
 			// 
 			// repackProgress
 			// 
-			repackProgress.Location = new Point(363, 418);
+			repackProgress.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			repackProgress.Location = new Point(519, 452);
+			repackProgress.Margin = new Padding(4, 5, 4, 5);
 			repackProgress.Name = "repackProgress";
-			repackProgress.Size = new Size(425, 23);
+			repackProgress.Size = new Size(320, 38);
 			repackProgress.TabIndex = 6;
+			// 
+			// splitContainer1
+			// 
+			splitContainer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			splitContainer1.Location = new Point(17, 79);
+			splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			splitContainer1.Panel1.Controls.Add(lvlsList);
+			// 
+			// splitContainer1.Panel2
+			// 
+			splitContainer1.Panel2.Controls.Add(outputDisplay);
+			splitContainer1.Size = new Size(822, 360);
+			splitContainer1.SplitterDistance = 386;
+			splitContainer1.TabIndex = 7;
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.ImageScalingSize = new Size(24, 24);
+			menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, aboutToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 0);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(856, 33);
+			menuStrip1.TabIndex = 8;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clearItemsToolStripMenuItem1, removeSelectedToolStripMenuItem, pickFolderToolStripMenuItem1, exitToolStripMenuItem });
+			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			fileToolStripMenuItem.Size = new Size(54, 29);
+			fileToolStripMenuItem.Text = "&File";
+			// 
+			// clearItemsToolStripMenuItem1
+			// 
+			clearItemsToolStripMenuItem1.Name = "clearItemsToolStripMenuItem1";
+			clearItemsToolStripMenuItem1.Size = new Size(249, 34);
+			clearItemsToolStripMenuItem1.Text = "&Clear Items";
+			clearItemsToolStripMenuItem1.Click += clearItemsToolStripMenuItem_Click;
+			// 
+			// removeSelectedToolStripMenuItem
+			// 
+			removeSelectedToolStripMenuItem.Name = "removeSelectedToolStripMenuItem";
+			removeSelectedToolStripMenuItem.Size = new Size(249, 34);
+			removeSelectedToolStripMenuItem.Text = "&Remove Selected";
+			removeSelectedToolStripMenuItem.Click += removeSelectedItemToolStripMenuItem_Click;
+			// 
+			// pickFolderToolStripMenuItem1
+			// 
+			pickFolderToolStripMenuItem1.Name = "pickFolderToolStripMenuItem1";
+			pickFolderToolStripMenuItem1.Size = new Size(249, 34);
+			pickFolderToolStripMenuItem1.Text = "&Pick Folder";
+			pickFolderToolStripMenuItem1.Click += pickFolder_Click;
+			// 
+			// exitToolStripMenuItem
+			// 
+			exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			exitToolStripMenuItem.Size = new Size(249, 34);
+			exitToolStripMenuItem.Text = "E&xit";
+			exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+			// 
+			// aboutToolStripMenuItem
+			// 
+			aboutToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { gitHubRepoToolStripMenuItem, aboutToolStripMenuItem1 });
+			aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			aboutToolStripMenuItem.Size = new Size(78, 29);
+			aboutToolStripMenuItem.Text = "&About";
+			// 
+			// gitHubRepoToolStripMenuItem
+			// 
+			gitHubRepoToolStripMenuItem.Name = "gitHubRepoToolStripMenuItem";
+			gitHubRepoToolStripMenuItem.Size = new Size(216, 34);
+			gitHubRepoToolStripMenuItem.Text = "&GitHub Repo";
+			gitHubRepoToolStripMenuItem.Click += gitHubRepoToolStripMenuItem_Click;
+			// 
+			// aboutToolStripMenuItem1
+			// 
+			aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+			aboutToolStripMenuItem1.Size = new Size(216, 34);
+			aboutToolStripMenuItem1.Text = "A&bout";
+			aboutToolStripMenuItem1.Click += aboutToolStripMenuItem1_Click;
 			// 
 			// Repack
 			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleDimensions = new SizeF(10F, 25F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
+			ClientSize = new Size(856, 505);
+			Controls.Add(splitContainer1);
+			Controls.Add(menuStrip1);
 			Controls.Add(repackProgress);
 			Controls.Add(preferCompressedTextures);
 			Controls.Add(repackButton);
 			Controls.Add(pickFolder);
-			Controls.Add(outputDisplay);
 			Controls.Add(label1);
-			Controls.Add(lvlsList);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			MainMenuStrip = menuStrip1;
+			Margin = new Padding(4, 5, 4, 5);
 			Name = "Repack";
 			Text = "lvl-repack";
+			contextMenu_lvlFiles.ResumeLayout(false);
+			splitContainer1.Panel1.ResumeLayout(false);
+			splitContainer1.Panel2.ResumeLayout(false);
+			splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+			splitContainer1.ResumeLayout(false);
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -130,5 +294,19 @@
 		private Button repackButton;
 		private CheckBox preferCompressedTextures;
 		private ProgressBar repackProgress;
+		private ContextMenuStrip contextMenu_lvlFiles;
+		private ToolStripMenuItem clearItemsToolStripMenuItem;
+		private ToolStripMenuItem removeSelectedItemToolStripMenuItem;
+		private ToolStripMenuItem pickFolderToolStripMenuItem;
+		private SplitContainer splitContainer1;
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem clearItemsToolStripMenuItem1;
+		private ToolStripMenuItem removeSelectedToolStripMenuItem;
+		private ToolStripMenuItem pickFolderToolStripMenuItem1;
+		private ToolStripMenuItem exitToolStripMenuItem;
+		private ToolStripMenuItem aboutToolStripMenuItem;
+		private ToolStripMenuItem gitHubRepoToolStripMenuItem;
+		private ToolStripMenuItem aboutToolStripMenuItem1;
 	}
 }
--- gui/Form1.cs
+++ gui/Form1.cs
@@ -12,12 +12,89 @@ namespace gui
 {
 	public partial class Repack : Form
 	{
-		List<string> files = new List<string>();
 
 		public Repack()
 		{
 			InitializeComponent();
+			var tip = new System.Windows.Forms.ToolTip();
+			tip.SetToolTip(lvlsList,
+				"Choose 'Pick Folder' or drag and drop .lvl files\n" +
+				"and/or folders containing .lvls onto the list."
+				);
 		}
+
+		// Add a file or folder to the lvls to process.
+		// if path is a folder, all .lvls found under the folder will be added.
+		private void AddPath(string path)
+		{
+			if (path != null)
+			{
+				if (Directory.Exists(path))
+				{
+					List<string> files = new List<string>();
+					files.AddRange(Directory.EnumerateFiles(path, "*.lvl", SearchOption.AllDirectories));
+					lvlsList.BeginUpdate();
+					foreach (var v in files)
+						lvlsList.Items.Add(v);
+					lvlsList.EndUpdate();
+				}
+				else if (File.Exists(path) && path.EndsWith(".lvl", StringComparison.InvariantCultureIgnoreCase))
+				{
+					lvlsList.Items.Add(path);
+				}
+			}
+			repackButton.Enabled = lvlsList.Items.Count > 0;
+		}
+
+		private void IncrementProgress()
+		{
+			if (this.InvokeRequired)
+			{
+				this.Invoke(new Action(() => IncrementProgress()));
+			}
+			else
+			{
+				repackProgress.Value++;
+			}
+		}
+
+		private void AddOutputMessage(string msg)
+		{
+			if (outputDisplay.InvokeRequired)
+			{
+				outputDisplay.BeginInvoke((MethodInvoker)delegate
+				{
+					AddOutputMessage(msg);
+				});
+			}
+			else
+			{
+				outputDisplay.AppendText(msg);
+			}
+		}
+
+		#region Drag/Drop handlers
+		// For drag/drop the control's 'AllowDrop' property needs to be set to true
+		private void lvlsList_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effect = DragDropEffects.Copy; // Show copy cursor
+			else
+				e.Effect = DragDropEffects.None; // Show no-drop cursor
+		}
+
+		private void lvlsList_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+			// Process the dropped files/folders here
+			foreach (string file in files)
+			{
+				AddPath(file);
+			}
+		}
+		#endregion Drag/Drop handlers
+
 
 		private void pickFolder_Click(object sender, EventArgs e)
 		{
@@ -42,15 +119,8 @@ namespace gui
 
 					try
 					{
-						files.AddRange(Directory.EnumerateFiles(new string(coFolderPath), "*.lvl", SearchOption.AllDirectories));
-
-						lvlsList.BeginUpdate();
-
-						foreach (var v in files) lvlsList.Items.Add(v);
-
-						lvlsList.EndUpdate();
-
-						repackButton.Enabled = true;
+						string path = new string(coFolderPath);
+						AddPath(path);
 					}
 					finally
 					{
@@ -65,12 +135,30 @@ namespace gui
 
 		private async void repackButton_Click(object sender, EventArgs e)
 		{
+			string repackExe = ".\\repack.exe";
+
+			if (!File.Exists(repackExe))
+			{
+				MessageBox.Show("'Repack.exe' not found!\nPlace 'repack.exe' in the same folder as 'lvl-repack.exe'.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			List<string> files = new List<string>();
+			foreach (var v in lvlsList.Items)
+				files.Add(v.ToString());
+
+			repackProgress.Enabled = true;
+			repackProgress.Minimum = 0;
+			repackProgress.Maximum = files.Count;
 
 			await Parallel.ForEachAsync(files, async (file, cancel) =>
 			{
-				string repackArgs = preferCompressedTextures.Checked ? $"{file} prefer_compessed_textures" : $"{file}";
+				string repackArgs = preferCompressedTextures.Checked ? $"\"{file}\" prefer_compessed_textures" : $"\"{file}\"";
+				long sizeBefore = new FileInfo(file).Length;
 
-				ProcessStartInfo startInfo = new ProcessStartInfo(".\\repack.exe", repackArgs);
+				string debugStr = $".\\repack.exe {repackArgs}";
+				System.Diagnostics.Debug.WriteLine(debugStr);
+				ProcessStartInfo startInfo = new ProcessStartInfo(repackExe, repackArgs);
 
 				startInfo.RedirectStandardOutput = true;
 				startInfo.RedirectStandardError = true;
@@ -91,7 +179,10 @@ namespace gui
 
 					if (process.ExitCode == 0)
 					{
-						message = await process.StandardOutput.ReadToEndAsync();
+						string output = await process.StandardOutput.ReadToEndAsync();
+						long sizeAfter = new FileInfo(file).Length;
+						double sizeReduced = (1.0 * sizeBefore - sizeAfter) / (1024.0 * 1024.0);
+						message = string.Format("[Reduced by {0:F2} MB] {1}", sizeReduced, output);
 					}
 					else
 					{
@@ -99,15 +190,53 @@ namespace gui
 					}
 				}
 
-				if (outputDisplay.InvokeRequired)
-				{
-					outputDisplay.Invoke(() => outputDisplay.AppendText(message));
-				}
-				else
-				{
-					outputDisplay.AppendText(file);
-				}
+				IncrementProgress();
+				AddOutputMessage(message);
 			});
+		}
+
+		private void clearItemsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			lvlsList.Items.Clear();
+			outputDisplay.Clear();
+			repackProgress.Value = 0;
+		}
+
+		private void removeSelectedItemToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			for (int i = lvlsList.SelectedIndices.Count - 1; i >= 0; i--)
+			{
+				lvlsList.Items.RemoveAt(lvlsList.SelectedIndices[i]);
+			}
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void gitHubRepoToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			string url = "https://github.com/PrismaticFlower/lvl-repack";
+			try
+			{
+				ProcessStartInfo psi = new ProcessStartInfo { FileName = url, UseShellExecute = true };
+				Process.Start(psi);
+			}
+			catch
+			{
+				MessageBox.Show("Error while opening: " + url);
+			}
+		}
+
+		private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show(
+				"lvl-repack lets you shrink the size of your Battlefront 1 and 2 .lvl\n" +
+				"files by reducing space taken by textures.\n" +
+				"Click 'Pick Folder' or\n" +
+				"drag and drop files and folders onto the lvl files list."
+				, "About lvl-repack");
 		}
 	}
 }
